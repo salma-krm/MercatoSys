d5ab821617b3b2599aaacfbf02b143ccd983edd6
salma <salmatergui7@gmail.com>
2025-11-24 23:51:33 +0100

    Initial project setup

---
+++ src/main/java/com/mercatosys/MercatoSysApplication.java
@@ -0,0 +1,13 @@
+package com.mercatosys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercatoSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MercatoSysApplication.class, args);
+    }
+
+}
